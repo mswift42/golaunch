35c4fdc68e0770604199c41fcb68c2f00ccb90ed
martin.haesler <martin.haesler@gmail.com>
2014-11-25 18:58:21 +0000

    add test for NewSearch

--- golaunch_test.go
+++ golaunch_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,4 +10,23 @@ import (
 func TestGetFileFromPath(t *testing.T) {
 	assert := assert.New(t)
 	assert.Equal(getFileFromPath("/home/severin/Documents/sicp.pdf"), "sicp")
+	assert.Equal(getFileFromPath("/home/severin/Documents/Dive Into Python.pdf"),
+		"Dive Into Python")
+}
+
+func TestNewResults(t *testing.T) {
+	assert := assert.New(t)
+	s := []string{"/home/severin/Documents/sicp.pdf", "/home/severin/Documents/Dive Into Python.pdf"}
+	nr := NewResults(s)
+	assert.Equal(nr[0].Name, "sicp")
+	assert.Equal(nr[1].Name, "Dive Into Python")
+	assert.Equal(nr[0].FullPath, "/home/severin/Documents/sicp.pdf")
+}
+func TestNewSearchFind(t *testing.T) {
+	find, _ := NewSearchFind("Lisp")
+	for _, i := range find.results {
+		if !strings.Contains(i.Name, "Sportschau") {
+			t.Errorf("expected lisp got nil")
+		}
+	}
 }
